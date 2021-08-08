433a4608e4473064bd3dbe3179966089ffd88f25
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-08 16:35:09 -0700

    Modified: getting dice roll number

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -2,8 +2,8 @@ package com.bridgelabz.snakeladder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to the snake ladder game");
-        int startPosition = 0;                                        // initialize startPosition to zero
-        System.out.println("Single Player Game Started At Position 0");
+        int startPosition = 0;                                           // initialize startPosition to zero
+        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);    // it will generate 1 to 6 number randomly
+        System.out.println("Dice Roll Number : "+ dieRoll);
     }
 }
