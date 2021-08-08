5c46b8bc9de705cebea243435c5ca967ae5978c4
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-08 16:23:35 -0700

    Modified: single player start with 0

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -3,5 +3,7 @@ package com.bridgelabz.snakeladder;
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the snake ladder game");
+        int startPosition = 0;                                        // initialize startPosition to zero
+        System.out.println("Single Player Game Started At Position 0");
     }
 }
